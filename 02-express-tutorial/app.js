a42f9f11753de6ad837b2e75a99bfa3e545cb2e0
Kazeem Amuzat Asojuomode <77699947+amuzatk@users.noreply.github.com>
2024-02-08 11:03:04 +0100

    new update

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,10 +1,19 @@
-// console.log('Express Tutorial')
 const http = require('http')
+const {readFileSync} = require('fs')
+
+const helloWorld = readFileSync('./index.html')
 
 const server = http.createServer((req,res)=>{
+    // console.log(req.url)
+    if(req.url === '/'){
     res.writeHead(200, {'content-type':'text/html'})
-    res.write('<h1>Home Page</h1>')
+    res.write(helloWorld)
     res.end()
+} else {
+    res.writeHead(200, { 'content-type': 'text/html'})
+    res.write('NOT FOUND')
+    res.end()
+}
 })
 
 server.listen(5000)
