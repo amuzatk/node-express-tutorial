b4fcac5a23e8460c370f6261f0d7853a77040203
kazeem amuzat <kazeem@paydestal.com>
2024-07-12 07:15:57 +0100

    new branch

--- 01-node-tutorial/02-globals.js
+++ 01-node-tutorial/02-globals.js
@@ -6,7 +6,10 @@
 // module     - info about current module (file)
 // process    - info about env where the program is being executed
 
-console.log(__dirname)
-setInterval(() => {
-  console.log('hello world')
-}, 1000)
+console.log(__dirname) // @ 0min this runs
+setTimeout(()=> { //after 1sec setTimeout runs
+  setInterval(() => { //after 3seconds first 'hello world' runs
+    console.log('hello world')
+  }, 2000)
+}, 1000);
+
