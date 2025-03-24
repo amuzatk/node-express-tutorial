cc7d378edfa9dad2210ed04c7ae7788d1d709818
Kazeem Amuzat <kazeem@paydestal.com>
2025-03-24 13:29:33 +0100

    first

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,21 +1,83 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-    // throw new Error("testing express error handler===")
-  const products = await Product.find({ price: { $gt: 30 } })
-    .sort('price')
-    .select('name price');
+    // const search = 'R';
+    // const products = await Product.find({}).sort("name price");
+    const products = await Product.find({price: {$gt: 30 }})
+    .sort('name')
+    .select("name price")
+    // .limit(5)
+    .skip(0)
+    ;
+//   const products = await Product.find({ price: { $gt: 30 } })
+//     .sort('price')
+//     .select('name price');
 
-  // res.status(200).json({ products, nbHits: products.length });
   res.status(200).json({products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const {featured} = req.query;
-  const queyObject = {};
+  const {featured, name, company, sort, fields, numericFilters} = req.query;
+  const queryObject = {};
   if(featured){
-    queyObject.featured = featured === "true" ? true : false;
+    queryObject.featured = featured === "true" ? true : false;
   }
-  const products = await Product.find(queyObject)
+  if (name) {
+    queryObject.name = {$regex:name, $options:'i' };
+  }
+  if (company) {
+    queryObject.company =company;
+  }
+
+  if(numericFilters){
+    const operatorMap = {
+        '<':'$lt',
+        '<=':'$lte',
+        '=':'$eq',
+        '>':'$gt',
+        '>=':'$gte',
+
+    }
+    const regeX = /\b(<|<=|=|>|>=)\b/g;
+    let filter = numericFilters.replace(regeX, (match)=> `-${operatorMap[match]}-` );
+
+    const options = ["price", "rating"];
+
+    filter = filter.split(',').forEach((item)=>{
+        const [field, operator, value] = item.split("-");
+        if(options.includes(field)){
+            queryObject[field] = {[operator] : Number(value) };
+        }
+    });
+    console.log(filter,'==');
+  }
+console.log(queryObject,'query==')
+
+  let result = Product.find(queryObject);
+
+  if(sort){
+  const sortList = sort.split(',').join(' ');
+  result = result.sort(sortList);
+// console.log(sort,'sort query==')
+    
+  } else {
+    result = result.sort('createdAt');
+  }
+
+  if(fields){
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+    // console.log(fields,'fields query==')
+  }
+
+const page = Number(req.query.page);
+const limit = Number(req.query.limit);
+const skip = (page - 1) * limit;
+
+result = result.skip(skip).limit(limit);
+// 23 total item in the response
+// limit=perPag=7 implies total page=4 (7 7 7 2);
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
 
   // const { featured, company, name, sort, fields, numericFilters } = req.query;
   // const queryObject = {};
@@ -74,8 +136,8 @@ const getAllProducts = async (req, res) => {
 
   // const products = await result;
 
-  console.log(queyObject, products.length,"====");
-  res.status(200).json({ products, nbHits: products.length });
+//   console.log(queryObject, products.length,"====");
+//   res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
