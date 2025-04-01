b4c2bfa9aade77b2ae682de05170ab602528bc99
Kazeem Amuzat <kazeem@paydestal.com>
2025-04-01 09:21:18 +0100

    yy

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -1,14 +1,24 @@
 const Job = require('../models/Job');
+const {StatusCodes} = require('http-status-codes');
+const {BadRequestError, NotFoundError} = require('../errors')
 
-
-
-const createJob = (req,res)=>{
-    // res.send("Create Job")
-    res.json(req.body)
+const createJob = async (req,res)=>{
+    // console.log(req.body,'body=');
+    // console.log(req.user,'user==');
+    // console.log(req.user.userId,'user.userId===');
+    // req.body.createdby = req.user.userId;
+    const newJob = await Job.create({
+        ...req.body,
+        createdBy: req.user.userId//req.user is within the auth middleware
+    })
+    res.status(StatusCodes.CREATED).json({msg:'Job Created Successfully', data: newJob})
 }
 
-const getJobs = (req,res)=>{
-    res.send("Get Jobs")
+const getJobs = async (req,res)=>{//fetch jobs of a user
+    // console.log(req.body,'jobs==')
+    // console.log(req.user,'req.user==')
+    const jobs = await Job.find({createdBy:req.user.userId}).sort('createdAt');
+    res.status(StatusCodes.OK).json({msg:'User Jobs Fetched Successfully', data: jobs, count: jobs.length})
 }
 
 const getJob = (req,res)=>{
